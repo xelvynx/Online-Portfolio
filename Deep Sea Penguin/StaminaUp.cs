7f21d409d1f438a955cfc71ee4a34c5248bbaafe
xelvynx <elvynleviste@gmail.com>
2019-10-19 12:28:26 -0700

    Add files via upload

---
+++ Deep Sea Penguin/StaminaUp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaminaUp : MonoBehaviour {
+
+
+    void Update()
+    {
+        transform.Translate(Vector3.down * Time.deltaTime*(8+GameManager.gm.addSpeed));
+    }
+}
